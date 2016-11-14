3cceaa8402730815dc57a1075d9563e4bd80dba6
jacksondunstan <jackson@jacksondunstan.com>
2016-11-13 21:50:11 -0800

    Add assert level classes and their helper

---
+++ AssertFast.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using UnityEngine.Assertions;
+using UnityEngine.Assertions.Comparers;
+
+[DebuggerStepThrough]
+public static class AssertFast
+{
+	public static bool raiseExceptions;
+
+	private static void Fail(string message, string userMessage)
+	{
+		if (Debugger.IsAttached)
+		{
+			throw new AssertionException(message, userMessage);
+		}
+		if (raiseExceptions)
+		{
+			throw new AssertionException(message, userMessage);
+		}
+		if (userMessage != null)
+		{
+			message = userMessage + '\n' + message;
+		}
+		UnityEngine.Debug.LogAssertion(message);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsTrue(bool condition)
+	{
+		IsTrue(condition, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsTrue(bool condition, string message)
+	{
+		if (!condition)
+		{
+			Fail(AssertionMessageUtil.BooleanFailureMessage(true), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsFalse(bool condition)
+	{
+		IsFalse(condition, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsFalse(bool condition, string message)
+	{
+		if (condition)
+		{
+			Fail(AssertionMessageUtil.BooleanFailureMessage(false), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreApproximatelyEqual(float expected, float actual)
+	{
+		AreEqual<float>(
+			expected,
+			actual,
+			null,
+			FloatComparer.s_ComparerWithDefaultTolerance
+		);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreApproximatelyEqual(float expected, float actual, string message)
+	{
+		AreEqual<float>(
+			expected,
+			actual,
+			message,
+			FloatComparer.s_ComparerWithDefaultTolerance
+		);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreApproximatelyEqual(float expected, float actual, float tolerance)
+	{
+		AreApproximatelyEqual(expected, actual, tolerance, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreApproximatelyEqual(
+		float expected,
+		float actual,
+		float tolerance,
+		string message
+	)
+	{
+		AreEqual<float>(expected, actual, message, new FloatComparer(tolerance));
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotApproximatelyEqual(float expected, float actual)
+	{
+		AreNotEqual<float>(
+			expected,
+			actual,
+			null,
+			FloatComparer.s_ComparerWithDefaultTolerance
+		);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotApproximatelyEqual(float expected, float actual, string message)
+	{
+		AreNotEqual<float>(
+			expected,
+			actual,
+			message,
+			FloatComparer.s_ComparerWithDefaultTolerance
+		);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotApproximatelyEqual(float expected, float actual, float tolerance)
+	{
+		AreNotApproximatelyEqual(expected, actual, tolerance, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotApproximatelyEqual(
+		float expected,
+		float actual,
+		float tolerance,
+		string message
+	)
+	{
+		AreNotEqual<float>(expected, actual, message, new FloatComparer(tolerance));
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreEqual<T>(T expected, T actual)
+	{
+		AreEqual<T>(expected, actual, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreEqual<T>(T expected, T actual, string message)
+	{
+		AreEqual<T>(expected, actual, message, EqualityComparer<T>.Default);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreEqual<T>(
+		T expected,
+		T actual,
+		string message,
+		IEqualityComparer<T> comparer
+	)
+	{
+		if (typeof(UnityEngine.Object).IsAssignableFrom(typeof(T)))
+		{
+			AreEqual(expected as UnityEngine.Object, actual as UnityEngine.Object, message);
+			return;
+		}
+		if (!comparer.Equals(actual, expected))
+		{
+			Fail(AssertionMessageUtil.GetEqualityMessage(actual, expected, true), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreEqual(
+		UnityEngine.Object expected,
+		UnityEngine.Object actual,
+		string message
+	)
+	{
+		if (actual != expected)
+		{
+			Fail(AssertionMessageUtil.GetEqualityMessage(actual, expected, true), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotEqual<T>(T expected, T actual)
+	{
+		AreNotEqual<T>(expected, actual, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotEqual<T>(T expected, T actual, string message)
+	{
+		AreNotEqual<T>(expected, actual, message, EqualityComparer<T>.Default);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotEqual<T>(
+		T expected,
+		T actual,
+		string message,
+		IEqualityComparer<T> comparer
+	)
+	{
+		if (typeof(UnityEngine.Object).IsAssignableFrom(typeof(T)))
+		{
+			AreNotEqual(expected as UnityEngine.Object, actual as UnityEngine.Object, message);
+			return;
+		}
+		if (comparer.Equals(actual, expected))
+		{
+			Fail(AssertionMessageUtil.GetEqualityMessage(actual, expected, false), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void AreNotEqual(
+		UnityEngine.Object expected,
+		UnityEngine.Object actual,
+		string message
+	)
+	{
+		if (actual == expected)
+		{
+			Fail(AssertionMessageUtil.GetEqualityMessage(actual, expected, false), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsNull<T>(T value) where T : class
+	{
+		IsNull<T>(value, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsNull<T>(T value, string message) where T : class
+	{
+		if (typeof(UnityEngine.Object).IsAssignableFrom(typeof(T)))
+		{
+			IsNull(value as UnityEngine.Object, message);
+		}
+		else if (value != null)
+		{
+			Fail(AssertionMessageUtil.NullFailureMessage(value, true), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsNull(UnityEngine.Object value, string message)
+	{
+		if (value != null)
+		{
+			Fail(AssertionMessageUtil.NullFailureMessage(value, true), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsNotNull<T>(T value) where T : class
+	{
+		IsNotNull<T>(value, null);
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsNotNull<T>(T value, string message) where T : class
+	{
+		if (typeof(UnityEngine.Object).IsAssignableFrom(typeof(T)))
+		{
+			IsNotNull(value as UnityEngine.Object, message);
+		}
+		else if (value == null)
+		{
+			Fail(AssertionMessageUtil.NullFailureMessage(value, false), message);
+		}
+	}
+
+	#if UNITY_ASSERTIONS_DISABLE_FAST || !UNITY_ASSERTIONS
+	[Conditional("__NEVER_DEFINED__")]
+	#endif
+	public static void IsNotNull(UnityEngine.Object value, string message)
+	{
+		if (value == null)
+		{
+			Fail(AssertionMessageUtil.NullFailureMessage(value, false), message);
+		}
+	}
+}
---
+++ AssertionMessageUtil.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class AssertionMessageUtil
+{
+	public static string GetMessage(string failureMessage)
+	{
+		return string.Format("{0} {1}", "Assertion failed.", failureMessage);
+	}
+
+	public static string GetMessage(string failureMessage, string expected)
+	{
+		return AssertionMessageUtil.GetMessage(
+			string.Format(
+				"{0}{1}{2} {3}",
+				failureMessage,
+				Environment.NewLine,
+				"Expected:",
+				expected
+			)
+		);
+	}
+
+	public static string GetEqualityMessage(object actual, object expected, bool expectEqual)
+	{
+		return AssertionMessageUtil.GetMessage(
+			string.Format(
+				"Values are {0}equal.",
+				(!expectEqual) ? string.Empty : "not "
+			),
+			string.Format(
+				"{0} {2} {1}",
+				actual,
+				expected,
+				(!expectEqual) ? "!=" : "=="
+			)
+		);
+	}
+
+	public static string NullFailureMessage(object value, bool expectNull)
+	{
+		return AssertionMessageUtil.GetMessage(
+			string.Format(
+				"Value was {0}Null",
+				(!expectNull) ? string.Empty : "not "
+			),
+			string.Format(
+				"Value was {0}Null",
+				(!expectNull) ? "not " : string.Empty
+			)
+		);
+	}
+
+	public static string BooleanFailureMessage(bool expected)
+	{
+		return AssertionMessageUtil.GetMessage("Value was " + !expected, expected.ToString());
+	}
+}
